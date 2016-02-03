ac4f0e298bd08a520efee54b16457fcebd1f6fca
s-wakasa <wakasa.s1129@gmail.com>
2016-02-03 17:06:12 +0900

    文字列表示と四則演算の課題

---
+++ challenge_1/1_1-6challenge.php
@@ -0,0 +1,25 @@
+<?php
+
+echo 'hello world!!<br/>';
+//課題1
+
+$message ='groove';
+$message2 ='_';
+const MOJI ='gear';
+echo $message.$message2.MOJI.'<br/>';
+//課題2
+
+echo "私の名前は若狭です<br/>";
+const AGE ='23';
+echo AGE."歳です<br/>";
+//課題3
+
+$n1 = 9;
+$n2 = 5;
+const N1 = 3;
+const N2 = 6;
+$n1 = $n1 + N2;
+$n2 = $n2 * N2;
+echo $n1.'と'.$n2;
+//課題4と5
+
