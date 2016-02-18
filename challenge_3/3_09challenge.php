d254810e3c397c4132277d9bfb7c51b6e53af81f
s-wakasa <wakasa.s1129@gmail.com>
2016-02-18 17:33:38 +0900

    配列の変更に伴い分岐条件を修正

--- challenge_3/3_09challenge.php
+++ challenge_3/3_09challenge.php
@@ -1,15 +1,15 @@
 <?php
 
-$user111 = array('name' => "wakasa",'birth' => "1129",'address' => null); //addressをnullに
-$user222 = array('name' => "akiyama",'birth' => "0606",'address' => "ibaraki");
-$user333 = array('name' => "ansai",'birth' => "0923",'address' => "tochigi");
-
+$user111 = array('id' => "111",'name' => "wakasa",'birth' => "1129",'address' => null); //addressをnullに
+$user222 = array('id' => "222",'name' => "akiyama",'birth' => "0606",'address' => "ibaraki");
+$user333 = array('id' => "333",'name' => "ansai",'birth' => "0923",'address' => "tochigi");
+//課題6に伴いidのkeyを修正
 $alluser = array($user111,$user222,$user333);
 
 	foreach($alluser as $arr_value){
 		foreach($arr_value as $key => $value){
-			if($value == null){
-				continue; //nullの場合にスキップ
+			if($value == null||$key =='id'){
+				continue; //valueがnull、またはkeyがidの場合にechoしない
 			}
 		echo $key."=".$value."/";
 		}
--- challenge_3/3_10challenge.php
+++ challenge_3/3_10challenge.php
@@ -1,8 +1,8 @@
 <?php
-$user111 = array('name' => "wakasa",'birth' => "1129",'address' => null); //addressをnullに
-$user222 = array('name' => "akiyama",'birth' => "0606",'address' => "ibaraki");
-$user333 = array('name' => "ansai",'birth' => "0923",'address' => "tochigi");
-
+$user111 = array('id' => "111",'name' => "wakasa",'birth' => "1129",'address' => null); //addressをnullに
+$user222 = array('id' => "222",'name' => "akiyama",'birth' => "0606",'address' => "ibaraki");
+$user333 = array('id' => "333",'name' => "ansai",'birth' => "0923",'address' => "tochigi");
+//課題6に伴いidのkeyを修正
 $alluser = array($user111,$user222,$user333);
 $limit = 2;//ここに制限回数を入力
 $count = 0;
@@ -12,8 +12,8 @@ foreach($alluser as $arr_value){
 		break;
 	}
 	foreach($arr_value as $key => $value){
-		if($value == null){
-			continue; //nullの場合にスキップ
+		if($value == null||$key =='id'){
+			continue; //課題9に伴いcontinue分岐を修正
 		}
 		echo $key."=".$value."/";
 	}
