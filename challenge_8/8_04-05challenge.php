09d1d52397a3e41067dde9fb0be4295a2f98e900
s-wakasa <wakasa.s1129@gmail.com>
2016-02-24 17:49:47 +0900

    新規

---
+++ challenge_8/8_04-05challenge.php
@@ -0,0 +1,29 @@
+﻿<?php
+class penguin{
+	public $a=0; //カプセル化されたオブジェクトをプロパティと呼ぶらしい
+	public $b=0;
+	public function number(){
+		$this->a=8;
+		$this->b=14;
+	}
+	public function write(){
+		echo $this->a."<br/>".$this->b."<br/>";
+	}	
+}
+class karappo extends penguin{
+	public function reset(){
+		$this->a='';
+		$this->b='';
+	}
+}
+
+$emperor = new penguin();//penguinクラスでインスタンス
+echo $emperor->a.$emperor->b."<br/>";
+$emperor->number();
+$emperor->write();
+
+$kogata = new karappo();
+$kogata->reset();
+echo '---'.$kogata->a.$kogata->b.'---';
+
+//課題4-5
