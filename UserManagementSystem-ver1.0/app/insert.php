e0885eed60f85479947f22d7c2f133993fd36ae8
s-wakasa <wakasa.s1129@gmail.com>
2016-02-28 22:15:50 +0900

    SESSIONの参照を用いた初期値設定に問題があったので修正

--- UserManagementSystem-ver1.0/app/insert.php
+++ UserManagementSystem-ver1.0/app/insert.php
@@ -1,4 +1,5 @@
 <?php require_once '../common/defineUtil.php'; ?>
+<?php require_once '../common/scriptUtil.php'; ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -7,52 +8,52 @@
 </head>
 <body>
 <?php
-session_start(); ?>
+session_start();?>
     <form action="<?php echo INSERT_CONFIRM ?>" method="POST">
     名前:
-    <input type="text" name="name">
+    <input type="text" name="name" value=<?php keep_val('name','');?>>
     <br><br>
     
     生年月日:　
     <select name="year">
-        <option value=<?php echo $_SESSION['year'] ?>><?php echo $_SESSION['year'] ?></option>
+        <option value=<?php keep_val('year','----') ?>><?php keep_val('year','----') ?></option>
         <?php
         for($i=1950; $i<=2010; $i++){ ?>
         <option value="<?php echo $i;?>"><?php echo $i ;?></option>
         <?php } ?>
     </select>年
     <select name="month">
-        <option value=<?php echo $_SESSION['month'] ?>><?php echo $_SESSION['month'] ?></option>
+        <option value=<?php keep_val('month','--') ?>><?php keep_val('month','--') ?></option>
         <?php
         for($i = 1; $i<=12; $i++){?>
         <option value="<?php echo $i;?>"><?php echo $i;?></option>
         <?php } ;?>
     </select>月
     <select name="day">
-        <option value=<?php echo $_SESSION['day'] ?>><?php echo $_SESSION['day'] ?></option>
+        <option value=<?php keep_val('day','--'); ?>><?php keep_val('day','--'); ?></option>
         <?php
         for($i = 1; $i<=31; $i++){ ?>
         <option value="<?php echo $i; ?>"><?php echo $i;?></option>
-        <?php } ?><!--これだとセッションが空の場合にエラーが出る、各初期値に分岐を作る必要がある-->>
+        <?php } ?><!--SESSIONの値の有無で分岐を作るとアクセス初回の参照でエラーが出たのでPOSTを使うことにした-->
     </select>日
     <br><br>
 
     種別:
     <br>
-    <input type="radio" name="type" value="エンジニア"　checked>エンジニア<br>
-    <input type="radio" name="type" value="営業">営業<br>
-    <input type="radio" name="type" value="その他">その他<br>
-    <br>
+    <input type="radio" name="type" value="エンジニア" <?php if(isset($_POST['type'])){if($_POST['type']=='エンジニア'){echo 'checked';}}?>>エンジニア<br>
+    <input type="radio" name="type" value="営業" <?php if(isset($_POST['type'])){if($_POST['type']=='営業'){echo 'checked';}}?>>営業<br>
+    <input type="radio" name="type" value="その他" <?php if(isset($_POST['type'])){if($_POST['type']=='その他'){echo 'checked';}}?>>その他<br>
+    <br><!-- 各ボタンとPOSTの値が一致するボタンにchecked -->
     
     電話番号:
-    <input type="text" name="tell">
+    <input type="text" name="tell" value=<?php keep_val('tell','');?>>
     <br><br>
 
     自己紹介文
     <br>
-    <textarea name="comment" rows=10 cols=50 style="resize:none" wrap="hard"><?php echo $_SESSION['comment']?></textarea><br><br>
+    <textarea name="comment" rows=10 cols=50 style="resize:none" wrap="hard"><?php keep_val('comment','')?></textarea><br><br>
     
     <input type="submit" name="btnSubmit" value="確認画面へ">
-    </form><!--回答4.初期値をセッションから取得、入力項目を維持する-->
+    </form><!--回答4.初期値をinsert_confirm.phpのPOSTから取得、入力項目を維持する-->
 </body>
 </html>
